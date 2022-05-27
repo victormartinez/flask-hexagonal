2ee7c3f74e96081e88c2898e9fb0ca4d442423bd
Victor Martinez <victor.martinez@lend.tech>
2022-05-27 08:09:48 -0300

    feat: implement templates repositories

--- flask_hexagonal/infrastructure/database/repositories/templates.py
+++ flask_hexagonal/infrastructure/database/repositories/templates.py
@@ -1,18 +1,52 @@
-from typing import List
+from typing import List, Type
+from uuid import UUID
 
+from sqlalchemy.future import select
+
+from flask_hexagonal.infrastructure.database import models, session
 from flask_hexagonal.domain.templates import (
-    TemplateRepositoryInterface,
+    ListTemplateRepositoryInterface,
+    RetrieveTemplateRepositoryInterface,
+    PersistTemplateRepositoryInterface,
     TemplateInterface,
 )
+from flask_hexagonal.infrastructure.database.models.templates import DBTemplate
 
 
-class DBTemplateRepository(TemplateRepositoryInterface):
+class RetrieveDBTemplateRepository(RetrieveTemplateRepositoryInterface):
 
-    def get(self, idx: str) -> TemplateInterface:
-        raise NotImplementedError("'TemplateRepositoryInterface.get' must be implemented.")
+    def _init__(self, Session: Type[session.Session]):
+        self.Session = Session
+
+    def get(self, idx: UUID) -> TemplateInterface:
+        # TODO: handle errors
+        query = select(models.DBTemplate).filter(models.DBTemplate.id == idx)
+        with self.Session.begin() as db_session:
+            result = db_session.execute(query)
+            return result.scalars().first()
+
+
+class ListDBTemplateRepository(ListTemplateRepositoryInterface):
+
+    def _init__(self, Session: Type[session.Session]):
+        self.Session = Session
 
     def list(self) -> List[TemplateInterface]:
-        raise NotImplementedError("'TemplateRepositoryInterface.list' must be implemented.")
+        # TODO: handle errors
+        with self.Session.begin() as db_session:
+            query = select(models.DBTemplate)
+            result = db_session.execute(query)
+            return result.scalars().all()
+
+
+class PersistDBTemplateRepository(PersistTemplateRepositoryInterface):
+
+    def _init__(self, Session: Type[session.Session]):
+        self.Session = Session
 
     def insert(self, name: str, tokens: List[str], external_id: str) -> TemplateInterface:
-        raise NotImplementedError("'TemplateRepositoryInterface.insert' must be implemented.")
+        with self.Session.begin() as db_session:
+            template = DBTemplate(name=name, tokens=tokens, external_id=external_id)
+            db_session.add(template)
+            db_session.commit()
+            return template
