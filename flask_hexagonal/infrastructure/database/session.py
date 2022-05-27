3e9a3e8d30c8f113a652db3ea6370dc3235947d6
Victor Martinez <victor.martinez@lend.tech>
2022-05-27 08:08:44 -0300

    chore: implement db session maker

---
+++ flask_hexagonal/infrastructure/database/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import settings
+
+
+engine = create_engine(settings.build_database_uri())
+Session = sessionmaker(engine)
