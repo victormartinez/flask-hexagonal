31efec0edbbac39a1d8ab85af710ddc8ddf640e9
Victor Martinez <victor.martinez@lend.tech>
2022-05-27 08:10:19 -0300

    fix: make service take minor repositories

--- flask_hexagonal/domain/templates/service.py
+++ flask_hexagonal/domain/templates/service.py
@@ -5,8 +5,11 @@ from .exceptions import (
     TokenValidationError,
 )
 from .interfaces import (
-    ExternalTemplateRepositoryInterface,
-    TemplateRepositoryInterface,
+    ListExternalTemplateTokensRepositoryInterface,
+    RetrieveExternalTemplateRepositoryInterface,
+    ListTemplateRepositoryInterface,
+    RetrieveTemplateRepositoryInterface,
+    PersistTemplateRepositoryInterface,
     ExternalTemplateInterface,
     TemplateInterface,
     TemplateServiceInterface,
@@ -15,20 +18,16 @@ from .interfaces import (
 
 class TemplateService(TemplateServiceInterface):
     
-    def __init__(self, repository: TemplateRepositoryInterface, external_repository: ExternalTemplateRepositoryInterface):
-        self.repository = repository
-        self.external_repository = external_repository
+    def list_templates(self, repository: ListTemplateRepositoryInterface) -> List[TemplateInterface]:
+        return repository.list()
 
-    def list_templates(self) -> List[TemplateInterface]:
-        return self.repository.list()
+    def get_template(self, id: str, repository: RetrieveTemplateRepositoryInterface) -> TemplateInterface:
+        return repository.get()
 
-    def get_template(self, id: str) -> TemplateInterface:
-        return self.repository.get()
-
-    def import_template(self, external_id: str) -> TemplateInterface:
+    def import_template(self, external_id: str, external_repository: RetrieveExternalTemplateRepositoryInterface, repository: PersistTemplateRepositoryInterface) -> TemplateInterface:
         try:
-            external_template: ExternalTemplateInterface = self.external_repository.get(external_id)
-            return self.repository.insert(
+            external_template: ExternalTemplateInterface = external_repository.get(external_id)
+            return repository.insert(
                 external_template.name,
                 external_template.tokens,
                 external_template.id,
@@ -36,8 +35,8 @@ class TemplateService(TemplateServiceInterface):
         except Exception as exc:
             raise TemplateImportError("", [])
 
-    def validate_tokens(self, external_id: str, requested_tokens: List[str]) -> None:
-        template_tokens = self.external_repository.list_tokens(external_id)
+    def validate_tokens(self, external_id: str, requested_tokens: List[str], external_repository: ListExternalTemplateTokensRepositoryInterface) -> None:
+        template_tokens = external_repository.list_tokens(external_id)
         missing_tokens = set(template_tokens).difference(requested_tokens)
         if len(missing_tokens) > 0:
             raise TokenValidationError("", [])
