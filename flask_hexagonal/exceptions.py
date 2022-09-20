33362b17a646fe3f558a5c802c5c8d46237e78a3
Victor Martinez <victor.martinez@lend.tech>
2022-09-19 23:04:14 -0300

    fix: depth recursion when exc raised

--- flask_hexagonal/exceptions.py
+++ flask_hexagonal/exceptions.py
@@ -13,25 +13,13 @@ class BaseException(Exception):
     def message(self) -> str:
         raise NotImplementedError("'Exception.message' not implemented.")
 
-    @message.setter
-    def message(self, message: str) -> None:
-        self.message = message
-
     @property
     def type(self) -> ExceptionType:
         raise NotImplementedError("'Exception.details' not implemented.")
 
-    @type.setter
-    def type(self, type: ExceptionType) -> None:
-        self.type = type
-
     @property
     def details(self) -> Optional[Dict[str, str]]:
         raise NotImplementedError("'Exception.details' not implemented.")
-
-    @details.setter
-    def details(self, details: Optional[Dict[str, str]]) -> None:
-        self.details = details
 
 
 class FlaskHexagonalException(BaseException):
@@ -41,7 +29,19 @@ class FlaskHexagonalException(BaseException):
         message: str,
         details: Optional[Dict[str, str]] = None,
     ):
-        self.type = type
-        self.message = message
-        self.details = details or {}
+        self._type = type
+        self._message = message
+        self._details = details or {}
         super().__init__(message)
+
+    @property
+    def message(self) -> str:
+        return self._message
+
+    @property
+    def type(self) -> ExceptionType:
+        return self._type
+
+    @property
+    def details(self) -> Optional[Dict[str, str]]:
+        return self._details
